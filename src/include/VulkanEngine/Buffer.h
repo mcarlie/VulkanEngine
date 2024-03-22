d6993eb075a671ed1924cb542608c81a77b3fa36
Michael Carlie <mcarlie92@gmail.com>
2024-03-22 19:24:06 +0900

    fix windows build

--- src/include/VulkanEngine/Buffer.h
+++ src/include/VulkanEngine/Buffer.h
@@ -6,7 +6,9 @@
 #ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnullability-completeness"
+#endif
 #include <vk_mem_alloc.h>
+#ifdef __clang__
 #pragma clang diagnostic pop
 #endif
 
