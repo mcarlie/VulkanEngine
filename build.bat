b05455eef6d2510a6e68b19add759608c8d361e1
Michael Carlie <mcarlie92@gmail.com>
2024-12-12 22:11:32 +0900

    simplify windows build script

--- build.bat
+++ build.bat
@@ -21,54 +21,12 @@ if not exist "vcpkg" (
     echo vcpkg directory already exists. Skipping clone.
 )
 
-REM Bootstrap vcpkg
-echo Bootstrapping vcpkg...
 cd vcpkg
 call .\bootstrap-vcpkg.bat
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Failed to bootstrap vcpkg.
-    exit /b 1
-)
-
-REM Integrate vcpkg with the environment
-echo Integrating vcpkg with the environment...
 .\vcpkg integrate install
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: vcpkg integration failed.
-    exit /b 1
-)
-
-REM Install required packages
-echo Installing dependencies via vcpkg...
 .\vcpkg install glfw3 eigen3 glslang vulkan
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Failed to install required dependencies.
-    exit /b 1
-)
-
-REM Set Vulkan SDK environment variable
-echo Setting VULKAN_SDK environment variable...
 set "VULKAN_SDK=%cd%\installed\x64-windows"
-
-REM Return to the project directory
 cd ..
 if not exist "build" mkdir build
-
-REM Run CMake with the vcpkg toolchain file
-echo Running CMake to configure the project...
 cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%cd%\vcpkg\scripts\buildsystems\vcpkg.cmake
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: CMake configuration failed.
-    exit /b 1
-)
-
-REM Build the project
-echo Building the project...
 cmake --build build
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Build process failed.
-    exit /b 1
-)
-
-echo Build complete!
-pause
